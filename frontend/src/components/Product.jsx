ef72419d9b7c5bd6332b5d9ca032de4ee8774b5b
786-Asif <ariffaizsaif786@gmail.com>
2024-01-02 22:49:43 +0530

    manage image height

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -6,7 +6,7 @@ const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img src={product.image} variant='top' style={{height:"507px",width:"636"}} />
       </Link>
 
       <Card.Body>
