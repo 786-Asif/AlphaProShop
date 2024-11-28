6a685b3bf71ba95a84e10cdb7a51cb7dc57b76df
Mohammad Asif <106311793+786-Asif@users.noreply.github.com>
2024-11-28 12:07:10 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRuotes from "./routes/uploadRoutes.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js ";
 
 const port = process.env.PORT || 5000; // for running the server
 connectDB(); // connect to mongoDB.
